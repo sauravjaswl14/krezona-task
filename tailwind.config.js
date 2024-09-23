40dc3736fd971682c9b43c02a72d17237b71e905
Saurav <saurav.jaswl@gmail.com>
2024-09-23 11:33:06 +0545

    added breakpoint configs and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px",
+      "2xl": "1536px",
+    },
     extend: {
       colors: {
         background: "var(--background)",
